433160b0f6433fa5597c0b9e4863aa0fd28abcc4
Halil Cam <76750950+halillcam@users.noreply.github.com>
2026-05-18 15:44:21 +0300

    Delete Mini Urun Sistemi/methods directory

--- Mini Urun Sistemi/methods/methods.dart
+++
@@ -1,9 +0,0 @@
-abstract class IProductMethods {
-  void addProducts(int? productID, String? productName, int? productPrice, String? productStatus);
-  void deleteProducts(int? id);
-  void updateProductStatus(int? id, String? status);
-  void listProducts();
-  void searchWithName(String? productName);
-  void filterWithPrice(int? maxPrice);
-  void filterWithStatus(String? status);
-}
